8d48bdb01b7cbce5eecd828a84e12932af4e0eea
tomsqq <315802153@qq.com>
2015-08-06 15:55:32 +0800

    second commit

--- common-first/src/main/java/com/xiniunet/First.java
+++ common-first/src/main/java/com/xiniunet/First.java
@@ -6,7 +6,7 @@ package com.xiniunet;
 public class First {
 
     public static String hello(){
-        return "first";
+        return "firstsss";
     }
     public void helloa(){
         System.out.println("hello");
